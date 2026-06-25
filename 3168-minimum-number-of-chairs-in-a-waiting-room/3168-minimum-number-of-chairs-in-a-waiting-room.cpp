ac515a697b6a081c2b5b06bd6df8756a5fc05022
Abhinav <chauhanabhinav872@gmail.com>
2026-06-25 19:46:26 +0530

    Time: 0 ms (100%), Space: 8.8 MB (16.28%) - LeetHub

---
+++ 3168-minimum-number-of-chairs-in-a-waiting-room/3168-minimum-number-of-chairs-in-a-waiting-room.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumChairs(string s) {
+       int e = 0;
+       int l = 0;
+       int mx= 0;
+       for(int i=0; i<s.length(); i++){
+        if(s[i]=='E') e++;
+        else e--;
+        mx = max(mx, e);
+       }
+       return mx; 
+    }
+};
