3ff6e9cd8272a602fba0fb458e5d7eb407593f35
Abhinav <chauhanabhinav872@gmail.com>
2026-07-04 09:50:25 +0530

    Time: 0 ms (100%), Space: 17.7 MB (19.38%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,44 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-       vector<int> v = {-1,-1};
-    //    int lo=0;
-    //    int hi=nums.size()-1;
-    //    while(lo<=hi){
-    //     int mid = lo + (hi-lo)/2;
-    //     if(nums[mid]==target){
 
-    //     }
-       for(int i=0; i<nums.size(); i++){
-        if(nums[i]==target){
-            v[0]=i;
-            break;
+    int firstoccurence(vector<int>& nums, int target){
+        int lo = 0;
+        int hi = nums.size()-1;
+        int ans=-1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                hi = mid-1;
+            }
+            else if(nums[mid]<target) lo=mid+1;
+            else hi=mid-1;
         }
-       }
-       for(int j=nums.size()-1; j>=0; j--){
-        if(nums[j]==target){
-            v[1] = j;
-            break;
+        return ans;
+    }
+
+    int lastoccurence(vector<int>& nums, int target){
+        int lo = 0;
+        int hi = nums.size()-1;
+        int ans=-1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid]==target){
+                ans = mid;
+                lo = mid+1;
+            }
+            else if(nums[mid]<target) lo=mid+1;
+            else hi=mid-1;
         }
-       }
-       return v;
+        return ans;
+    }
+
+       
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>v(2);
+        v[0] = firstoccurence(nums, target);
+        v[1] = lastoccurence(nums,target);
+
+        return v;
     }
 };
