dc6da9c6ee127c9abea6805d63ef72ea4fc4de2f
Abhinav <chauhanabhinav872@gmail.com>
2026-07-28 16:37:43 +0530

    Time: 29 ms (32.13%), Space: 22.4 MB (96.64%) - LeetHub

---
+++ 2521-distinct-prime-factors-of-product-of-array/2521-distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void fillSieve(vector<bool>& sieve){
+        int n = sieve.size()-1;
+        for(int i=2; i<=sqrt(n); i++){
+            for(int j=i*2; j<=n; j+=i){
+                sieve[j]=0;
+            }
+        }
+    }
+    int distinctPrimeFactors(vector<int>& nums) {
+        int n=nums.size();
+        int mx = -1;
+        for(int i=0; i<n; i++){
+           mx = max(mx, nums[i]); 
+        }
+        vector<bool> seive(mx+1, 1);
+        if(mx+1>0) seive[0] = 0;
+        if(mx+1>1) seive[1] = 0;
+        
+        fillSieve(seive);
+        vector<int> primes;
+        for(int i=2; i<=mx; i++){
+            if(seive[i]==1) primes.push_back(i);
+        }
+        vector<bool> taken(primes.size(),0);
+        for(int i=0; i<nums.size(); i++){
+            int ele = nums[i];
+            for(int j=0; j<primes.size(); j++){
+                if(primes[j]>ele) break;
+                if(ele%primes[j]==0) taken[j]=1;
+            }
+        }
+        int count=0;
+        for(int i=0; i<taken.size();i++){
+           if(taken[i]==1) count++; 
+        }
+        return count;
+
+    }
+};
